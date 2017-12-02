4336cf8350cbbb2df08c6e8a2f84a36651e3f4ba
Brittany Morris <brittmmendez@gmail.com>
2017-12-02 00:00:31 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,9 @@ def my_find(collection)
     i = 0
     while i < collection.length
       return collection[i] if yield(collection[i])
+      i = i + 1
     end
-    i = i + 1
   end
-end
+
 collection=(1..100).to_a
 my_find(collection) {|1| 1 % 3 == 0 and 1 % 5}
